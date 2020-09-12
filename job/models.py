bcc66e033b7d48a7b562aec3ef1101cdfc451b94
Aleksandr Durnev <sandslash@bk.ru>
2020-09-12 21:59:09 +0400

    first commit

---
+++ job/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Occupation(models.Model):
+    name = models.CharField(max_length=200)
+    company_name = models.CharField(max_length=100)
+    position_name = models.CharField(max_length=100)
+    hire_date = models.DateTimeField(auto_created=True)
+    fire_date = models.DateTimeField(auto_created=True, default=None)
+    salary = models.IntegerField()
+    fraction = models.IntegerField()
+    base = models.IntegerField()
+    advance = models.IntegerField()
+    by_hours = models.BooleanField
